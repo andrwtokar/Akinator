df92301231420a0b78a14a84f7584c9933cba197
andrwtokar <44374062+andrwtokar@users.noreply.github.com>
2019-03-15 11:53:26 +0300

    Update akinator_list.h

--- akinator_list.h
+++ akinator_list.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>            //16-ричный редактор hexworkshop
+#include <locale.h>            //16-Р±РёС‚РѕРІС‹Р№ СЂРµРґР°РєС‚РѕСЂ hexworkshop
 #include <string>
 #include <iostream>
 #include <windows.h>
@@ -10,38 +10,38 @@
 //-----------STRUCTS-------------------------//
 struct Node
 {
-    std::string     text;
+	std::string     text;
 	Node* 	        YES;
 	Node* 	        NO;
 	Node*           Parent;
-	Node 	                        (std::string data);               //+
-	~Node 	                        ();                               //+
-	void	PG						();								  //+
-	void    Add_Node                ();                               //+
-	Node*   Find_Node               (std::string find_elem);          //+
-	void    Save                    (FILE* Play);                     //+
-	void    Read                    (FILE* Play);                     //+
+	Node 	                        	(std::string data);               //+
+	~Node 	                        	();                               //+
+	void		PG						();								  //+
+	void    	Add_Node                ();                               //+
+	Node*   	Find_Node               (std::string find_elem);          //+
+	void    	Save                    (FILE* Play);                     //+
+	void    	Read                    (FILE* Play);                     //+
 };
 //-------------------------------------------//
 struct TREE
 {
-    Node*           root;
-    TREE                            (Node* data);                     //+
-    ~TREE                           ();                               //+
-    int     Way_To_Node             (std::string element, int* arr);  //+
+	Node*           root;
+    	TREE                            (Node* data);                     //+
+    	~TREE                           ();                               //+
+   	 int     Way_To_Node             (std::string element, int* arr);  //+
 };
 //-------------------------------------------//
 struct Akinator
 {
-    TREE*           Play_Now;
-    Akinator                        ();                               //+
-    ~Akinator                       ();                               //+
-    void    Registrated_User        (std::string name);         	  //+
-    void    Object_Comporison       ();                               //+
-    void    Object_Definition       ();                               //+
-    void    Read_Tree               (std::string name);               //+
-    void    Save_Tree               (std::string name);               //+
-    void    Play                    ();                               //+
+    	TREE*           Play_Now;
+    	Akinator                        ();                               //+
+    	~Akinator                       ();                               //+
+    	void    Registrated_User        (std::string name);         	  //+
+    	void    Object_Comporison       ();                               //+
+    	void    Object_Definition       ();                               //+
+    	void    Read_Tree               (std::string name);               //+
+    	void    Save_Tree               (std::string name);               //+
+    	void    Play                    ();                               //+
 };
 //-----------Functions-----------------------//
 void    Interface                   ();                               //+
